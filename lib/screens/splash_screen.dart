81cda0acf07c39584061c40c49b97b6f8753554f
seongwook Choi <a4bingo3593@gmail.com>
2024-11-13 16:30:53 +0900

    splash init

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:appsolute_project_alco/screens/home/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,77 +7,9 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-
-    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
-      ..addListener(() {
-        setState(() {});
-      });
-
-    Future.delayed(const Duration(seconds: 2), () {
-      _controller.forward();
-    });
-
-    Future.delayed(const Duration(seconds: 4), () {
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const HomeScreen()),
-        );
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ClipRect(
-              child: Align(
-                alignment: Alignment.topCenter,
-                heightFactor: 0.8,
-                child: Image.asset(
-                  "assets/images/logo.png",
-                  width: 150,
-                  height: 150,
-                ),
-              ),
-            ),
-          ),
-          if (_animation.value > 0)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 120),
-              child: LinearProgressIndicator(
-                borderRadius: BorderRadius.circular(100),
-                value: _animation.value,
-                minHeight: 5,
-                backgroundColor: Colors.grey[300],
-                valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-              ),
-            ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }

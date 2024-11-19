6127f69814c4339cfb653d6fd45df1ff8ea02698
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-11-19 11:08:01 +0900

    your die

--- lib/screens/auth/sign_in_screen.dart
+++ lib/screens/auth/sign_in_screen.dart
@@ -12,98 +12,132 @@ class SignInScreen extends StatefulWidget {
   State<SignInScreen> createState() => _SignInScreenState();
 }
 
-class _SignInScreenState extends State<SignInScreen> with SingleTickerProviderStateMixin {
-
+class _SignInScreenState extends State<SignInScreen>
+    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const SizedBox(height: 50,),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 150),
-                child: Image.asset("assets/images/logo.png"),
+        resizeToAvoidBottomInset: false,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const SizedBox(
+                height: 50,
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
-              child: CustomTextFormField(hint: "아이디"),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
-              child: CustomTextFormField(hint: "비밀번호", obscureText: true,),
-            ),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 58,
-                  child: FilledButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Color(0xFF4A6ACA)),
-                    ),
-                    child: const Text("로그인", style: TextStyle(fontFamily: "Paperlogy",fontWeight: FontWeight.w800, fontSize: 16),),
-                  ),
-                )
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const CustomCheckBox(),
-                  const SizedBox(width: 10,),
-                  Text("자동로그인", style: TextStyle(fontFamily: "Paperlogy", fontSize: 15, fontWeight: FontWeight.w500, color: Colors.black.withOpacity(0.86)),)
-                ],
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 150),
+                  child: Image.asset("assets/images/logo.png"),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Spacer(),
-                  CustomTextBtn(text: "아이디 찾기", onPressed: () {}),
-                  Text("|", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, fontFamily: "Paperlogy"),),
-                  CustomTextBtn(text: "비밀번호 찾기", onPressed: () {}),
-                  Text("|", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, fontFamily: "Paperlogy"),),
-                  CustomTextBtn(text: "회원가입", onPressed: () {}),
-                  Spacer()
-                ],
+              const Padding(
+                padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+                child: CustomTextFormField(hint: "아이디"),
               ),
-            ),
-            Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 22),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          top: BorderSide(
-                              color: Colors.black.withOpacity(0.29)
-                          )
+              const Padding(
+                padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+                child: CustomTextFormField(
+                  hint: "비밀번호",
+                  obscureText: true,
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 58,
+                    child: FilledButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                        backgroundColor:
+                            WidgetStatePropertyAll(Color(0xFF4A6ACA)),
                       ),
-                      color: Colors.white
+                      child: const Text(
+                        "로그인",
+                        style: TextStyle(
+                            fontFamily: "Paperlogy",
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16),
+                      ),
+                    ),
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const CustomCheckBox(),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "자동로그인",
+                      style: TextStyle(
+                          fontFamily: "Paperlogy",
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black.withOpacity(0.86)),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    CustomTextBtn(text: "아이디 찾기", onPressed: () {}),
+                    Text(
+                      "|",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "Paperlogy"),
+                    ),
+                    CustomTextBtn(text: "비밀번호 찾기", onPressed: () {}),
+                    Text(
+                      "|",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "Paperlogy"),
+                    ),
+                    CustomTextBtn(text: "회원가입", onPressed: () {}),
+                    Spacer()
+                  ],
+                ),
+              ),
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 22),
+                    decoration: BoxDecoration(
+                        border: Border(
+                            top: BorderSide(
+                                color: Colors.black.withOpacity(0.29))),
+                        color: Colors.white),
                   ),
-                ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: Text("간편 로그인 연동", style: TextStyle(fontFamily: "Paperlogy", fontSize: 16, fontWeight: FontWeight.w200),),
-                ),
-              ],
-            )
-          ],
-        ),
-      )
-    );
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: Text(
+                      "간편 로그인 연동",
+                      style: TextStyle(
+                          fontFamily: "Paperlogy",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w200),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
